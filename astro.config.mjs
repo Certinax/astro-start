d4f3e365fc5d5c5d0f001cb1666f662ec3e61d14
Mathias Ahrn <golvkaldt@gmail.com>
2025-08-29 18:42:09 +0200

    testing relative links

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
+const isProd = process.env.NODE_ENV === "production";
 // https://astro.build/config
 export default defineConfig({
-  base: "/astro-start",
-  site: "https://certinax.github.io",
+  site: isProd ? "https://certinax.github.io" : "http://localhost:4321",
+  base: isProd ? "/astro-start" : "/",
   integrations: [mdx(), sitemap()],
 });
