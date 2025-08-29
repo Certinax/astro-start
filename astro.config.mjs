f2f35e1e5e2e155e96f502e338c63b776ceada44
Mathias Ahrn <golvkaldt@gmail.com>
2025-08-29 19:04:23 +0200

    mkay

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/astro-start/",
+  base: "/astro-start",
   site: "https://certinax.github.io/astro-start/",
   integrations: [mdx(), sitemap()],
 });
